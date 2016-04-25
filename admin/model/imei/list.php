06ecbe5965b7dca8c9bd119e6e30039bc9595d37
Erdal Mersinlioglu <erdal.mersinlioglu@gmail.com>
2016-04-25 20:26:24 +0200

    end of imei management

---
+++ admin/model/imei/list.php
@@ -0,0 +1,173 @@
+<?php
+class ModelImeiList extends Model {
+	public function addCategory($data) {
+
+		$imei_ids = array();
+		if (isset($data['nummers']) && is_array($data['nummers'])) {
+			foreach ($data['nummers'] as $nummer) {
+				if (strlen($nummer) == 0 /*&& strlen(nummer) != 15*/) {
+					continue;
+				}
+
+				$this->db->query("INSERT INTO " . DB_PREFIX . "imei SET nummer = '" . $nummer . "';");
+				$imei_ids[] = $this->db->getLastId();
+			}
+		}
+
+		return $imei_ids;
+	}
+
+	public function editCategory($id, $data) {
+
+		$this->db->query("UPDATE " . DB_PREFIX . "imei SET nummer = '" . $data['nummer'] . "' WHERE id = '" . (int)$id . "'");
+
+		$this->cache->delete('imei');
+	}
+
+	public function deleteCategory($category_id) {
+		//$this->event->trigger('pre.admin.category.delete', $category_id);
+
+		$this->db->query("DELETE FROM " . DB_PREFIX . "imei WHERE id = '" . (int)$category_id . "'");
+
+		$this->cache->delete('imei');
+
+		//$this->event->trigger('post.admin.category.delete', $category_id);
+	}
+
+	public function repairCategories($parent_id = 0) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category WHERE parent_id = '" . (int)$parent_id . "'");
+
+		foreach ($query->rows as $category) {
+			// Delete the path below the current one
+			$this->db->query("DELETE FROM `" . DB_PREFIX . "category_path` WHERE category_id = '" . (int)$category['category_id'] . "'");
+
+			// Fix for records with no paths
+			$level = 0;
+
+			$query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "category_path` WHERE category_id = '" . (int)$parent_id . "' ORDER BY level ASC");
+
+			foreach ($query->rows as $result) {
+				$this->db->query("INSERT INTO `" . DB_PREFIX . "category_path` SET category_id = '" . (int)$category['category_id'] . "', `path_id` = '" . (int)$result['path_id'] . "', level = '" . (int)$level . "'");
+
+				$level++;
+			}
+
+			$this->db->query("REPLACE INTO `" . DB_PREFIX . "category_path` SET category_id = '" . (int)$category['category_id'] . "', `path_id` = '" . (int)$category['category_id'] . "', level = '" . (int)$level . "'");
+
+			$this->repairCategories($category['category_id']);
+		}
+	}
+
+	public function getCategory($id) {
+		$query = $this->db->query("SELECT DISTINCT * FROM " . DB_PREFIX . "imei where id = '" . (int)$id . "'");
+
+		return $query->row;
+	}
+
+	public function getCategories($data = array()) {
+		$sql = "SELECT * FROM " . DB_PREFIX . "imei ";
+
+//		if (!empty($data['filter_name'])) {
+//			$sql .= " AND cd2.name LIKE '" . $this->db->escape($data['filter_name']) . "%'";
+//		}
+
+		$sort_data = array(
+			'nummer',
+			'id'
+		);
+
+		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
+			$sql .= " ORDER BY " . $data['sort'];
+		} else {
+			$sql .= " ORDER BY nummer";
+		}
+
+		if (isset($data['order']) && ($data['order'] == 'DESC')) {
+			$sql .= " DESC";
+		} else {
+			$sql .= " ASC";
+		}
+
+		if (isset($data['start']) || isset($data['limit'])) {
+			if ($data['start'] < 0) {
+				$data['start'] = 0;
+			}
+
+			if ($data['limit'] < 1) {
+				$data['limit'] = 20;
+			}
+
+			$sql .= " LIMIT " . (int)$data['start'] . "," . (int)$data['limit'];
+		}
+
+		$query = $this->db->query($sql);
+
+		return $query->rows;
+	}
+
+	public function getCategoryDescriptions($category_id) {
+		$category_description_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category_description WHERE category_id = '" . (int)$category_id . "'");
+
+		foreach ($query->rows as $result) {
+			$category_description_data[$result['language_id']] = array(
+				'name'             => $result['name'],
+				'meta_title'       => $result['meta_title'],
+				'meta_description' => $result['meta_description'],
+				'meta_keyword'     => $result['meta_keyword'],
+				'description'      => $result['description']
+			);
+		}
+
+		return $category_description_data;
+	}
+
+	public function getCategoryFilters($category_id) {
+		$category_filter_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category_filter WHERE category_id = '" . (int)$category_id . "'");
+
+		foreach ($query->rows as $result) {
+			$category_filter_data[] = $result['filter_id'];
+		}
+
+		return $category_filter_data;
+	}
+
+	public function getCategoryStores($category_id) {
+		$category_store_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category_to_store WHERE category_id = '" . (int)$category_id . "'");
+
+		foreach ($query->rows as $result) {
+			$category_store_data[] = $result['store_id'];
+		}
+
+		return $category_store_data;
+	}
+
+	public function getCategoryLayouts($category_id) {
+		$category_layout_data = array();
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "category_to_layout WHERE category_id = '" . (int)$category_id . "'");
+
+		foreach ($query->rows as $result) {
+			$category_layout_data[$result['store_id']] = $result['layout_id'];
+		}
+
+		return $category_layout_data;
+	}
+
+	public function getTotalCategories() {
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "imei");
+
+		return $query->row['total'];
+	}
+	
+	public function getTotalCategoriesByLayoutId($layout_id) {
+		$query = $this->db->query("SELECT COUNT(*) AS total FROM " . DB_PREFIX . "category_to_layout WHERE layout_id = '" . (int)$layout_id . "'");
+
+		return $query->row['total'];
+	}	
+}
